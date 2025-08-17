19fa3aa11334db8f093de7c114602f82b90c071a
mmiskatul <masabimiskat@gmail.com>
2025-08-17 08:31:02 +0600

    add getrecomendedUser function

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,32 @@
+import User from "../models/User.js";
+
+export async function getRecommendedUsers(req,res){
+    try {
+        const currentUserId =req.user.id;
+        const currentUser =req.user
+
+        const recomendeddUsers =await User.find({
+            $and:[
+                {_id:{$ne:currentUserId}},//excude myself
+                {$id:{$nin:currentUser.friends}},
+                {isOnboarded:true}
+
+            ]
+        })
+        res.status(200).json(recomendeddUsers)
+    } catch (error) {
+        console.error('Error in getRecommendeduser Controller',error.message);
+        res.status(500).json({
+            message:"Server Error"
+        })
+    }
+}
+
+
+export async function getMyFriends(req,res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,7 +1,14 @@
 
 import express from 'express'
+import { protectRoute } from '../middleware/auth.midleware.js';
+import { getMyFriends, getRecommendedUsers } from '../controllers/user.controller.js';
 
 const router =express.Router();
 
+// apply auth middle to all routes
+router.use(protectRoute)
+
+router.get('/',getRecommendedUsers);
+router.get('/friends',getMyFriends);
 
 export default router
