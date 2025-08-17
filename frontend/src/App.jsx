5d63c95a004810d2d482759d24cf4519b6866cf4
mmiskatul <masabimiskat@gmail.com>
2025-08-17 21:47:06 +0600

    add the tank stack query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import {Routes,Route} from 'react-router'
+
+import HomePage from './Pages/HomePage'
+import LoginPage from './Pages/LoginPage'
+import SignUpPage from './Pages/SignUpPage'
+import OnboardingPage from './Pages/OnboardingPage'
+import Callpage from './Pages/Callpage'
+import ChatPage from './Pages/ChatPage'
+
+import {Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div className=''>
+    <div className='h-screen ' data-theme ="night">
       <Routes>
-        
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/onboarding" element={<OnboardingPage/>}/>
+        <Route path="/notification" element={<Notification/>}/>
+        <Route path="/call" element={<Callpage/>}/>
+        <Route path="/chat" element={<ChatPage/>}/>
       </Routes>
+      <Toaster/>
     </div>
   )
 }
