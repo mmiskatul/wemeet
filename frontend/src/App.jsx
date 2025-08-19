646f7d1c00aa24e09d34411d8c65f955681d90ae
mmiskatul <masabimiskat@gmail.com>
2025-08-19 10:23:50 +0600

    signup from add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
@@ -11,26 +11,25 @@ import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "./lib/axios";
 
 const App = () => {
-  const { data } = useQuery({
-    queryKey: ["todos"],
+  const { data:authData, isLoading ,error } = useQuery({
+    queryKey: ["authUser"],
 
     queryFn: async () => {
       const res = await axiosInstance.get("/auth/me");
       return res.data;
     },
   });
-
-  console.log(data);
+  const authUser=authData?.user
   return (
     <div className="h-screen " data-theme="night">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
-        <Route path="/notification" element={<Notification />} />
-        <Route path="/call" element={<Callpage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage /> :<Navigate to="/"/>} />
+        <Route path="/onboarding" element={authUser ?<OnboardingPage /> : <Navigate to="/"/>} />
+        <Route path="/notification" element={authUser ? <Notification /> :<Navigate to="/"/>} />
+        <Route path="/call" element={authUser?<Callpage />:<Navigate to="/"/>} />
+        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/"/>} />
       </Routes>
       <Toaster />
     </div>
--- frontend/src/Pages/SignUpPage.jsx
+++ frontend/src/Pages/SignUpPage.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ShipWheel } from "lucide-react";
 
 const SignUpPage = () => {
-  return (
-    <div>
-      SignUpPage
-    </div>
-  )
-}
+  const [signUpData, setSignUpData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-export default SignUpPage
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log("Sign Up Data:", signUpData);
+  };
+
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        
+        {/* SIGNUP FORM -- LEFT SIDE */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          
+          {/* LOGO */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheel className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">We Meet</span>
+          </div>
+
+          {/* FORM */}
+          <form onSubmit={handleSignUp} className="flex flex-col gap-4">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="input input-bordered w-full"
+              value={signUpData.fullName}
+              onChange={(e) =>
+                setSignUpData({ ...signUpData, fullName: e.target.value })
+              }
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered w-full"
+              value={signUpData.email}
+              onChange={(e) =>
+                setSignUpData({ ...signUpData, email: e.target.value })
+              }
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full"
+              value={signUpData.password}
+              onChange={(e) =>
+                setSignUpData({ ...signUpData, password: e.target.value })
+              }
+            />
+            <button type="submit" className="btn btn-primary w-full">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
